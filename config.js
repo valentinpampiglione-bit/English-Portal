4cc470010361363111efef19cbc4264171826e3e
valentinpampiglione-bit <valentin.pampiglione@mi.unc.edu.ar>
2026-07-23 20:13:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 
 const SHEET_URLS = {
   students: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrDtsuNVA3UiCANsrXHRK0o7kwGl0vJwm3hgVZh8ZQ0Q5uBMRWFpNthgqzuhgmysspkUnVeiMcZ8VU/pub?gid=0&single=true&output=csv",
-  vocabulary: "PASTE_YOUR_VOCABULARY_SHEET_CSV_LINK_HERE",
+  vocabulary: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrDtsuNVA3UiCANsrXHRK0o7kwGl0vJwm3hgVZh8ZQ0Q5uBMRWFpNthgqzuhgmysspkUnVeiMcZ8VU/pub?gid=2001851899&single=true&output=csv",
   tasks: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrDtsuNVA3UiCANsrXHRK0o7kwGl0vJwm3hgVZh8ZQ0Q5uBMRWFpNthgqzuhgmysspkUnVeiMcZ8VU/pub?gid=798672785&single=true&output=csv",
   materials: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrDtsuNVA3UiCANsrXHRK0o7kwGl0vJwm3hgVZh8ZQ0Q5uBMRWFpNthgqzuhgmysspkUnVeiMcZ8VU/pub?gid=1137609035&single=true&output=csv",
 };
